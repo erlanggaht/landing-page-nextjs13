df576aebfb3d450a443a8c6b96c567e99a7c49ba
erlanggaht <erlanggahidayat.md@gmail.com>
2023-03-14 00:38:07 +0700

    update cuys ah

--- src/app/components/OurProduct/our-prouduct.js
+++ src/app/components/OurProduct/our-prouduct.js
@@ -5,7 +5,7 @@ import Card from './Card'
 export default function OurProuduct() {
   return (
     <div id='our-product' className={style.ourProduct}>
-      <h1 className={style.h1}>Our product</h1>
+      <h1 className={style.h1} id='titleOurProduct'>Our product</h1>
       <div id='our-product-card' className={style.ourProductCard}> 
       <Card/>
       <Card/>
