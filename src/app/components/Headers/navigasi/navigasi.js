c3491c807634a791c627f0ce0a678fe0a6c35989
erlanggaht <erlanggahidayat.md@gmail.com>
2023-03-15 12:44:29 +0700

    okelah

--- src/app/components/Headers/navigasi/navigasi.js
+++ src/app/components/Headers/navigasi/navigasi.js
@@ -1,25 +1,24 @@
 'use client'
-import { useRef} from "react";
+import { useRef, useState} from "react";
 import style from "../../../page.module.css";
 
 export default function Navigasi() {
   const ref = useRef()
-  
-  
- 
+  const [navMenu,setNavMenu] = useState(false)
 
+  function MenuHandle() {
+    setNavMenu(!navMenu ? true : false)
 
-
-  
+  }
 
   return (
     <div id="navigasi" className={style.navigasi}>
       <div id="title-menuNav" className={style.titlemenuNav}>
         <span className={style.title}>Erlanggaht.</span>
         <div id="listMenu" className={style.listMenu}>
-         <span className={style.arrowMobile} style={{fontSize:"1.4rem",display:"inline-block",fontWeight:"bolder"}}>{'>'}</span> 
+          <span className={style.arrowMobile} style={{fontSize:"1.3rem",display:"inline-block",fontWeight:"bolder",transform:`rotate(${navMenu ? '0deg' : '90deg'})`}} onClick={() => MenuHandle()}> {'>'}</span> 
         </div>
-        <div id="listMenu" className={style.listMenu} ref={ref}>
+        <div id="listMenu" className={!navMenu ? style.navigasiHidden : style.listMenu} ref={ref}>
           <span className={style.menu}>Home</span>
           <span className={style.menu}>About</span>
           <span className={style.menu}>Contact</span>
