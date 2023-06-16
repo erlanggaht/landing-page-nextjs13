f4da217c629b018acfa3d24c8fb4e886249fac8e
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-17 00:39:40 +0700

    ya

--- src/app/components/OurProduct/our-prouduct.js
+++ src/app/components/OurProduct/our-prouduct.js
@@ -7,7 +7,7 @@ export default function OurProuduct() {
     <div id='our-product' className={style.ourProduct}>
       <h1 className={style.h1} id='titleOurProduct'>Project</h1>
       <div id='our-product-card' className={style.ourProductCard}> 
-      <Card title={'Youtube Downloader'} description ={'Website download video youtube gratis mudah'} link={'https://youtdownloader.vercel.app/'}/>
+      <Card title={'Youtube Downloader'} description ={'Website download video youtube gratis mudah'} link={'https://youtubedownload.vercel.app/'}/>
       <Card  title={'Quran Web'} description ={'Website baca al-quran online'} link={'https://erlanggaht.github.io/webQuran/'}/>
       <Card  title={'Navigasi Web Sederhana'} description ={'Code HTML CSS navigasi sederhana'} link={'https://erlanggaht.github.io/project-testing/Project%20Navigasi/'}/>
       
